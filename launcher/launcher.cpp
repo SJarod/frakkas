184aff83082092b93e5478b0760506811d76bf89
f.marcellot <f.marcellot@student.isartdigital.com>
2022-03-18 14:23:58 +0000

    Feature/systems architecture

---
+++ launcher/launcher.cpp
@@ -0,0 +1,24 @@
+//
+// Created by f.marcellot on 18/03/2022.
+//
+
+#include <iostream>
+
+int main()
+{
+
+    bool running = true;
+    while(running)
+    {
+        /// NEW FRAME
+
+        /// UPDATE
+        // Engine::EntityManager::UpdateAndRender()
+
+        /// ENDFRAME
+
+        getchar();
+        running = false;
+    }
+    return 0;
+}
