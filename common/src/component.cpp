eb3668a9f0f0a455c7817613367af6d86f2ba0f1
f.marcellot <f.marcellot@student.isartdigital.com>
2022-03-19 16:56:41 +0000

    Added base EntityManager, and Component class

---
+++ common/src/component.cpp
@@ -0,0 +1,29 @@
+//
+// Created by flori on 3/17/2022.
+//
+
+#include "component.hpp"
+
+void Component::Enable() {
+    if (!enabled)
+    {
+        enabled = true;
+        OnEnable();
+    }
+}
+
+void Component::Disable() {
+    if (enabled)
+    {
+        enabled = false;
+        OnDisable();
+    }
+}
+
+void Component::SetOwner(Entity *owner) {
+    this->owner = owner;
+}
+
+bool Component::IsEnabled() {
+    return enabled;
+}
--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,24 +1,27 @@
 
 #include <iostream>
 
+#include "Engine/entity_manager.hpp"
 #include "Editor/editor_render.hpp"
-
-#include "Engine/foo.hpp"
 
 int main() {
 
-    Engine::SayHello();
+    Engine::EntityManager::Init();
 
     bool running = true;
+    bool gaming = false;
     while(running)
     {
         /// NEW FRAME
+        std::cout << std::endl <<"New Frame" << std::endl;
 
         /// UPDATE
         Editor::UpdateAndRender();
 
-        /// ENDFRAME
+        if (gaming)
+            Engine::EntityManager::UpdateAndRender();
 
+        /// ENDFRAME
         running = false;
     }
 
